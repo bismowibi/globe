60db716b3a8fef435a521ad161eafb61f1394093
Bismo Wibisono <bismowibi@gmail.com>
2021-11-17 22:10:49 +0700

    set up urql provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createClient, Provider } from 'urql';
+import './index.css';
+import App from './App';
+
+const client = createClient({
+  url: 'https://glorious-grub-83.hasura.app/v1/graphql',
+  fetchOptions: {
+    headers: {
+      'x-hasura-admin-secret':
+        '5rDuLhhJfExDevIi8JoLafItLYX36TtbpLHC1z17ZcO4epWzccDfV5aRp5ogMJBS',
+    },
+  },
+});
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-)
+  <React.StrictMode>
+    <Provider value={client}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
